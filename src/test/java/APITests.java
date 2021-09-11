499771d358bbeb674b82da1925fbf64a54c563f4
mwickramanayaka <mwickraman@wiley.com>
2021-09-11 23:55:18 +0530

    get api test

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,5 +1,6 @@
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class APITests {
@@ -7,13 +8,26 @@ public class APITests {
     @Test
     void test1(){
 
-        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        Response response = get("https://reqres.in/api/users?page=2");
 
         System.out.println("Response : " + response.asString());
         System.out.println("Status Code is : " + response.getStatusCode());
         System.out.println("Body : " + response.getBody().asString());
         System.out.println("Time Taken in ms: " + response.getTime());
         System.out.println("Header : " + response.getHeader("content-type"));
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode,200);
+
+    }
+
+    @Test
+    void test2(){
+
+        given().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                statusCode(200);
 
     }
 
